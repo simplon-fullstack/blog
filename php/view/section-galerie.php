ca43db2067e7e828328e35223e62af59b5f2d2d5
Your Name <you@example.com>
2019-09-04 15:06:20 +0200

    ajout album photo sur page galerie

--- php/view/section-galerie.php
+++ php/view/section-galerie.php
@@ -1,3 +1,20 @@
 <section>
     <h3>Galerie</h3>
+    <div class="listePhoto">
+
+<?php
+// AFFICHER LES IMAGES DU DOSSIER assets/album1/
+// récupérer la liste des fichiers
+$tabPhoto = glob("assets/album1/*.jpeg");
+// parcourir le tableau des fichiers
+foreach($tabPhoto as $photo)
+{
+    echo
+<<<CODEHTML
+    <img src="$photo" alt="$photo" title="$photo">
+CODEHTML;
+
+}
+?>
+    </div>
 </section>
